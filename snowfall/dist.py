afe11141b3f5e9f417cfebabc66d9dc7c40c9496
Daniel Povey <dpovey@gmail.com>
2021-04-10 23:37:49 +0800

    Change port..

--- snowfall/dist.py
+++ snowfall/dist.py
@@ -5,7 +5,7 @@ from torch import distributed as dist
 
 def setup_dist(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
+    os.environ['MASTER_PORT'] = '12354'
     dist.init_process_group("nccl", rank=rank, world_size=world_size)
     torch.cuda.set_device(rank)
 
